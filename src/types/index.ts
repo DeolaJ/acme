e9ab360042cbc7bfd6aa6bb4e4a9782a0df21fea
Adeola Adeyemo <adeola.adeyemoj@yahoo.com>
2023-12-23 13:37:03 +0100

    feat: create types
    Signed-off-by: Adeola Adeyemo <adeola.adeyemoj@yahoo.com>

---
+++ src/types/index.ts
@@ -0,0 +1,74 @@
+export type NavLink = {
+    id: string;
+    text: string;
+    url?: string;
+};
+
+export type ButtonLink = {
+    id: string;
+    text: string;
+    size?: "sm" | "md" | "lg";
+    url?: string;
+};
+
+export type YoutubeEmbed = {
+    __typename: "YoutubeEmbed";
+    id: string;
+    videoId: string;
+    start?: number;
+};
+
+type FeatureImage = {
+    __typename: "FeatureImage";
+    id: string;
+    image: {
+        id: string;
+        url: string;
+        width: number;
+        height: number;
+    };
+};
+
+export type Feature = {
+    __typename: "Feature";
+    id: string;
+    title: string;
+    description: string;
+    media: YoutubeEmbed | FeatureImage;
+    label?: string;
+    featureButton?: ButtonLink;
+};
+
+export type Callout = {
+    __typename: "Callout";
+    id: string;
+    title: string;
+    description: string;
+    calloutButton: ButtonLink;
+};
+
+export type Hero = {
+    id: string;
+    title: string;
+    description: string;
+    mobileTitle: string;
+    mobileDescription: string;
+    actionButtons: ButtonLink[];
+};
+
+export type Block = Feature | Callout;
+
+export type Section = {
+    id: string;
+    blocks: Block[];
+};
+
+export type Page = {
+    id: string;
+    title: string;
+    description: string;
+    heroSection: Hero;
+    headerNavigationLinks: NavLink[];
+    sections: Section[];
+    footerNavigationLinks: NavLink[];
+};
